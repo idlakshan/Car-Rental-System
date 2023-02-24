f57182308eebafd9190853dd121884bb4307af41
idlakshan <id.lakshan21@gmail.com>
2023-02-24 09:26:01 +0530

    admin dashboard create

--- FrontEnd/assets/js/navigation.js
+++ FrontEnd/assets/js/navigation.js
@@ -7,4 +7,25 @@ $("#btnReturn").click(function (){
     $("#landingPage").css('display','block');
     $("#guestUserPage").css('display','none');
 
-})
+});
+
+$("#btnSubmit").click(function (){
+    let UserName = $("#txtUserName").val();
+    let password = $("#txtPassword").val();
+
+    if (UserName=="dimuthu" && password=="aaa"){
+       /// alert("hri");
+        $("#adminPage").css('display','block');
+        $("#dashBoard").css('display','block')
+        $("#landingPage").css('display','none');
+    }else if (UserName=="" && password==""){
+        alert("Missing Fields")
+    }
+
+});
+
+$("#btnDashboard").click(function (){
+    $("#dashBoard").css('display','block');
+    $("#landingPage").css('display','none');
+
+});
